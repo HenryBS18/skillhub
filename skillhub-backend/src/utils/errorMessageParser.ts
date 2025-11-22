26cdab8d2eaacbe62986242856d1ec4d37d2f994
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-22 19:14:41 +0800

    Create error message parser

---
+++ skillhub-backend/src/utils/errorMessageParser.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express'
+import { NotFoundError } from 'src/errors/notFoundError'
+
+export const errorMessageParser = (res: Response, error: unknown) => {
+  if (error instanceof NotFoundError) {
+    return res.status(404).json({
+      message: error.message
+    })
+  }
+
+  if (error instanceof Error) {
+    return res.status(400).json({
+      message: error.message
+    })
+  }
+}
