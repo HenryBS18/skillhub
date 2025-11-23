c41ea3712839fcc257eedd8adcec31cfb840a965
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-23 15:30:15 +0800

    Create class page

--- skillhub-frontend/app/routes/class.tsx
+++ skillhub-frontend/app/routes/class.tsx
@@ -1,8 +1,50 @@
+import { ClassService } from "~/services"
+import type { Route } from "./+types/class"
 
-export default function Class() {
+const classService = new ClassService()
+
+export async function loader() {
+  return await classService.getAll()
+}
+
+export default function Class({ loaderData }: Route.ComponentProps) {
   return (
-    <main>
-      class
+    <main className="p-6">
+      <button className="btn btn-success">Tambah Kelas</button>
+
+      <div className="mt-4 overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID</th>
+              <th>Nama</th>
+              <th>Deskripsi</th>
+              <th>Instruktor</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              loaderData.map((classData, index) => (
+                <tr key={classData.id}>
+                  <th>{index + 1}</th>
+                  <td>{classData.id}</td>
+                  <td>{classData.name}</td>
+                  <td>{classData.description}</td>
+                  <td>{classData.instructor}</td>
+                  <td className="flex space-x-3">
+                    <button className="btn btn-info">Detail</button>
+                    <button className="btn btn-warning">Edit</button>
+                    <button className="btn btn-error">Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
