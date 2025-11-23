232e149aef809b493874307d4329cd4a7c9848f1
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-23 15:26:58 +0800

    Create student page

--- skillhub-frontend/app/routes/student.tsx
+++ skillhub-frontend/app/routes/student.tsx
@@ -1,8 +1,50 @@
+import { StudentService } from "~/services"
+import type { Route } from "./+types/student"
 
-export default function Student() {
+const studentService = new StudentService()
+
+export async function loader() {
+  return await studentService.getAll()
+}
+
+export default function Student({ loaderData }: Route.ComponentProps) {
   return (
-    <main>
-      student
+    <main className="p-6">
+      <button className="btn btn-success">Tambah Peserta</button>
+
+      <div className="mt-4 overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID</th>
+              <th>Nama</th>
+              <th>NIM</th>
+              <th>Email</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              loaderData.map((student, index) => (
+                <tr key={student.id}>
+                  <th>{index + 1}</th>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.nim}</td>
+                  <td>{student.email}</td>
+                  <td className="flex space-x-3">
+                    <button className="btn btn-info">Detail</button>
+                    <button className="btn btn-warning">Edit</button>
+                    <button className="btn btn-error">Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
