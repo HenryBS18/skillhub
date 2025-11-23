57e0efcc335ebdb259094e65113fc240a11405cc
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-23 13:48:09 +0800

    Create student service

---
+++ skillhub-frontend/app/services/student.service.ts
@@ -0,0 +1,102 @@
+import { Api, checkError } from "~/utils"
+
+type Student = {
+  id: string
+  name: string
+  nim: string
+  email: string
+}
+
+type CreateStudent = Omit<Student, 'id'>
+
+type AssignClass = {
+  studentId: string
+  classId: string
+}
+
+export class StudentService {
+  private api: Api
+
+  constructor() {
+    this.api = new Api('/student')
+  }
+
+  public async create(student: CreateStudent): Promise<void> {
+    try {
+      const res = await this.api.post('', student)
+      await checkError(res)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async getAll(): Promise<Student[]> {
+    try {
+      const res = await this.api.get('')
+      await checkError(res)
+
+      return await res.json()
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async getById(id: string): Promise<Student> {
+    try {
+      const res = await this.api.get(`/${id}`)
+      await checkError(res)
+
+      return res.json()
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async getAllClassById(id: string): Promise<any[]> {
+    try {
+      const res = await this.api.get(`/${id}/class`)
+      await checkError(res)
+
+      return res.json()
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async updateById(student: Student): Promise<void> {
+    try {
+      const res = await this.api.patch(`/${student.id}`, student)
+      await checkError(res)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async deleteById(id: string): Promise<void> {
+    try {
+      const res = await this.api.delete(`/${id}`)
+      await checkError(res)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async assignClass({ studentId, classId }: AssignClass): Promise<void> {
+    try {
+      const res = await this.api.post(`/${studentId}/class`, classId)
+      await checkError(res)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public async removeFromClass(id: string): Promise<void> {
+    try {
+      const api = new Api()
+      const res = await api.delete(`/student-class/${id}`)
+      await checkError(res)
+    } catch (error) {
+      throw error
+    }
+  }
+}
