15b2b561c2769af7cb21beb83f0edfb61a93b88d
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-24 23:00:10 +0800

    Create seed data

--- skillhub-backend/src/common/prisma.service.ts
+++ skillhub-backend/src/common/prisma.service.ts
@@ -1,10 +1,10 @@
 
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleInit } from '@nestjs/common'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '../../generated/prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaMariaDb({
       host: process.env.DB_HOST,
@@ -14,5 +14,61 @@ export class PrismaService extends PrismaClient {
     })
 
     super({ adapter })
+  }
+
+  async onModuleInit() {
+    await this.class.createMany({
+      skipDuplicates: true,
+      data: [
+        {
+          id: '5wpPSYIeyowA0pHMgOTf7',
+          name: 'Desain Grafis',
+          description: 'Kelas ini akan membimbing Anda dari nol untuk mengubah ide kreatif menjadi desain visual profesional, mulai dari membuat logo dasar hingga menyusun tata letak media cetak dan digital.',
+          instructor: 'Budi',
+        },
+        {
+          id: 'c0GW5wo9_TNoolX4s709L',
+          name: 'Pemrograman Dasar',
+          description: 'Kelas Pemrograman Dasar adalah kursus pengantar yang mengajarkan logika, algoritma, dan sintaks dasar dari bahasa pemrograman untuk membangun fondasi yang kuat dalam rekayasa perangkat lunak.',
+          instructor: 'Asep',
+        },
+        {
+          id: 'dff-_ptphH7VFr0z4QODg',
+          name: 'Editing Video',
+          description: 'Kelas ini akan membimbing Anda menguasai teknik dan perangkat lunak editing standar industri untuk mengubah rekaman mentah menjadi narasi visual yang sinematik dan menarik.',
+          instructor: 'Joko',
+        },
+        {
+          id: 'mRZrqb0ZE1l15zCWhzJuw',
+          name: 'Public Speaking',
+          description: 'Kelas ini mengajarkan teknik komunikasi yang efektif, membangun rasa percaya diri, dan keterampilan untuk menyampaikan ide dengan jelas dan meyakinkan di hadapan audiens.',
+          instructor: 'Wawans',
+        },
+      ]
+    })
+
+    await this.student.createMany({
+      skipDuplicates: true,
+      data: [
+        {
+          id: 'OZrP924wMQCWuwTF0kaeU',
+          name: 'Henry',
+          nim: '123',
+          email: 'henry@mail.com',
+        },
+        {
+          id: '4x3hEh8RV_lCK-DK4x-tR',
+          name: 'Hendra',
+          nim: '456',
+          email: 'hendra@mail.com',
+        },
+        {
+          id: '1lByGS2FwA-Z6IFVCKbGY',
+          name: 'Dewa',
+          nim: '789',
+          email: 'dewa@mail.com',
+        },
+      ]
+    })
   }
 }
