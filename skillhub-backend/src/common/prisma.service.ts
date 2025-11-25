9b5e04f1f24f6621fa50d53dcc76b6a21e3023e7
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2025-11-25 11:30:27 +0800

    Update adapter

--- skillhub-backend/src/common/prisma.service.ts
+++ skillhub-backend/src/common/prisma.service.ts
@@ -11,9 +11,10 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
     })
 
-    super({ adapter })
+    super({ adapter, errorFormat: "pretty", log: ["error", "info"] })
   }
 
   async onModuleInit() {
